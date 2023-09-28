4564ff4b62b3c484b8f21daec67f199c06a6d78f
Kobraz <roar.f.hanssen@gmail.com>
2023-09-28 18:11:04 +0200

    mad a new login1.js

---
+++ scripts/login1.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = e.currentTarget[1].value;
+  const password = e.currentTarget[2].value;
+
+  fetch(`https://api.noroff.dev/api/v1/social/auth/login?email=${email}&password=${password}`, {
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error("Failed to fetchdata from API");
+      }
+    })
+    .then((data) => {
+      if (data.exists) {
+        console.log("Data exists:", data);
+      } else {
+        console.log("Data does not exist");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
