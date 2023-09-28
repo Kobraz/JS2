2a792eab4bc293f9b05adc57cc8bdac3ad1c0e26
Kobraz <roar.f.hanssen@gmail.com>
2023-09-28 17:41:59 +0200

    changed method from get to post in login.js

--- scripts/login.js
+++ scripts/login.js
@@ -11,7 +11,7 @@ form.addEventListener("submit", function (e) {
   };
 
   fetch("https://api.noroff.dev/api/v1/social/auth/login", {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
